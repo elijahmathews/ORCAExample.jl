0b5b4ef80b8453d414e9c35e83238198b4cea800
Elijah Mathews <me@elijahmathews.com>
2022-05-19 14:55:38 -0400

    add docstring

--- src/ORCAExample.jl
+++ src/ORCAExample.jl
@@ -1,5 +1,16 @@
 module ORCAExample
 
+"""
+    greet()
+
+Display a "hello world" greeting to the user.
+
+# Examples
+```julia-repl
+julia> ORCAExample.greet()
+Hello World!
+```
+"""
 greet() = print("Hello World!")
 
 end # module
